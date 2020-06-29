403d22679d9602706b14a197428c51a70e89c108
unknown <matthewpiedra1228@gmail.com>
2020-06-29 15:44:01 -0400

    Added handle methods to control data from user input

--- src/App.js
+++ src/App.js
@@ -2,18 +2,38 @@ import React, {Component} from 'react';
 
 class Forms extends Component {
   constructor() {
-     super();
+    super();
 
-     this.state = {
-       
-     }
+    this.state = {
+      username: "",
+      password: "",
+    }
+  }
+
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  handleSubmit = event => {
+    event.preventDefault(); // this is stop propagation
+    alert(`An input was submitted: Username: ${this.state.username} Password: ${this.state.password}`);
   }
   
   render() {
     return (
-      <div>
-
-      </div>
+      <form onSubmit={event => this.handleSubmit(event)}>
+         <label htmlFor="name">
+            Name: 
+            <input name="username" onChange={this.handleChange} value={this.state.username} type="text" placeholder="Type in your name"/>
+         </label>
+         <label htmlFor="password">
+            Password:
+             <input type="password" name="password" value={this.state.password} onChange={this.handleChange}/>
+         </label>
+         <input type="submit"/>
+      </form>
     );
   }
 }
